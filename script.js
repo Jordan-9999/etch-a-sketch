f7e6e188efa2ca7274bce3ad38e3c139207095d4
jordan <jsheeler90@gmail.com>
2024-07-28 22:08:34 -0400

    added container.remove() when button is clicked, a new container is created and appended in same spot, loop to create div boxes is run, and new container is assigned to 'container' so it deletes and makes new grid every time.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-const container = document.querySelector("#container");
+let container = document.querySelector("#container");
 
 let squaresPerSide = 16;
 
@@ -11,23 +11,44 @@ newGridBtn.textContent = "click here";
 document.body.insertBefore(newGridBtn, container);
 
 newGridBtn.addEventListener("click", function(){
+
+
+
     squaresPerSide = prompt();
     
-    
-});
-
+    container.remove();
+    let newContainer = document.createElement("div");
+    newContainer.classList.add("new-container");
+    document.body.appendChild(newContainer);
 
     for (let i = 0; i < squaresPerSide; i++){
-        const squareRow = document.createElement("div");
-        squareRow.classList.add("square-row");
-        container.appendChild(squareRow);
+        const squareColumn = document.createElement("div");
+        squareColumn.classList.add("square-column");
+        newContainer.appendChild(squareColumn);
         
         for (let i = 0; i < squaresPerSide; i++){
             const squareChild = document.createElement("div");
             squareChild.classList.add("square-child");
-            squareRow.appendChild(squareChild);
-        }
+            squareColumn.appendChild(squareChild);
+        
+    }}
 
-    }
+    container = newContainer;
+    
+});
 
+
+
+    for (let i = 0; i < squaresPerSide; i++){
+        const squareColumn = document.createElement("div");
+        squareColumn.classList.add("square-column");
+        container.appendChild(squareColumn);
+        
+        for (let i = 0; i < squaresPerSide; i++){
+            const squareChild = document.createElement("div");
+            squareChild.classList.add("square-child");
+            squareColumn.appendChild(squareChild);
+        
+    }}
+    
     
